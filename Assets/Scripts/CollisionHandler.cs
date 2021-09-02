4f5956b72801fec62b6389e505d1728102592763
Michael <mike.brown11311@gmail.com>
2021-09-02 11:44:30 -0400

    Fixed Sound issues

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,23 +1,73 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float delay = 2f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
     private void OnCollisionEnter(Collision other) {
+        if(isTransitioning)
+            return;
+        
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("You hit a friendly object");
                 break;
-            case "Fuel":
-                Debug.Log("You hit a fuel refiller");
-                break;
             case "Finish":
-                Debug.Log("You finished the level");
+                SuccessSequence();
                 break;
             default:
-                Debug.Log("You hit an obstacle and took damage");
+                CrashSequence();
                 break;
         }
+    }
+
+    void SuccessSequence(){
+        //todo add particle effect on Success
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", delay);
+    }
+
+    void CrashSequence(){
+        //todo add particle effect on crash
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delay);
+    }
+
+    void ReloadLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        GetComponent<Movement>().enabled = false;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings){
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
     
 }
